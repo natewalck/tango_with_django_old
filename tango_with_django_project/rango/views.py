ad48b5bd1b8b08b3bf7f0af1962a99863393efcc
natewalck <nate@osxadm.in>
2013-12-24 17:23:07 -0500

    Add support for dynamic content in index view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,10 +4,14 @@ from django.http import HttpResponse
 from django.template import RequestContext 
 from django.shortcuts import render_to_response
 
+# Import the Category model
+from rango.models import Category
+
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    catergory_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': catergory_list}
     return render_to_response('rango/index.html', context_dict, context)
     # return HttpResponse("Rango says hello world! <a href='/rango/about'>About</a>")
 
